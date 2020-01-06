32cc640d80bc305ad77294afdce467fbc05f2ada
Rogério Filho <rogeriofilho632@gmail.com>
2020-01-06 10:22:15 -0300

    feat: functional part

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './components/Header';
 import PostList from './components/PostList';
+import Header from './components/Header';
 
+render(<Header />, document.getElementById('primary-nav'));
 render(<PostList />, document.getElementById('content'));
-/*under development */
